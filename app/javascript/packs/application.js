c90cf9372dc3a5c8f2906c4e8b797db64eb8f168
seanluse41 <seanluse41@gmail.com>
2021-01-05 22:31:10 +0900

    real time html streaming

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+//import Rails from "@rails/ujs"
 import "@hotwired/turbo-rails"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
+//Rails.start()
 ActiveStorage.start()
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+    validates :title, :body, :image, :link, presence: true
+    after_create_commit { broadcast_prepend_to "projects" }
+    after_update_commit { broadcast_replace_to "projects" }
+    after_destroy_commit { broadcast_remove_to "projects" }
 end
